8d4c8c5629553a23c99d1abeeb39925382055b50
onenueng <saipin.plu@mahidol.edu>
2022-06-08 13:02:51 +0700

    controller

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Bed;
+use App\Models\Procedure;
+use App\Models\Booking;
+
+class BookingController extends Controller
+{
+    // CRUD => create(GET create | POST store), read (GET resource/{id} show) update delete
+    public function create()
+    {
+        return view('booking')->with([
+            'beds'=> Bed::all(),
+            'procedures'=> Procedure::all()
+        ]);
+    }
+
+    public function store()
+    {
+        $data = request()->all();
+        $currentdate = date('Y-m-d');
+
+        // validate
+        //check booking date back
+        if ($data['datetime_start'] < $currentdate){
+            return back()->with('feedback', 'จองวันที่ย้อนหลังไม่ได้')->withInput();
+        }
+
+        //insert time to date
+        if ($data['time'] == 'morning'){
+            $datetime_start = $data['datetime_start'] . ' ' . '09:00:00';
+            $datetime_stop  = $data['datetime_start'] . ' ' . '12:00:00';
+        } else {
+            $datetime_start = $data['datetime_start'] . ' ' . '13:00:00';
+            $datetime_stop  = $data['datetime_start'] . ' ' . '16:00:00';
+        }
+
+        //check bed & datetime
+        $bookAlready = Booking::where('bed_id',$data['bed_id'])->where('datetime_start', $datetime_start)->count();
+
+
+        if ($bookAlready > 0){
+            return back()->with('feedback', 'เตียงนี้ถูกจองแล้ว')->withInput();
+
+        }
+
+        $booking = new Booking();
+        $booking->patient_id = 1;
+        $booking->bed_id = $data['bed_id'];
+        $bed = Bed::find($data['bed_id']);
+        $booking->room_id = $bed->room->id;
+        $booking->procedure_id = $data['procedure_id'];
+        $procedure = Procedure::find($data['procedure_id']);
+        $booking->clinic_id = $procedure->clinic->id;
+        $booking->datetime_start = $datetime_start;
+        $booking->datetime_stop = $datetime_stop;
+
+        // week_day
+        $booking->week_day = now()->parse($data['datetime_start'])->weekDay();
+        $booking->user_id = 1;
+        $booking->save(); //insert data
+
+        return redirect()->route('home')->with('feedback', 'จองเตียงสำเร็จแล้ว');
+    }
+
+    public function show($id)
+    {
+        return Booking::find($id);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Bed;
-use App\Models\Booking;
 use App\Models\Procedure;
 use App\Models\Clinic;
 use App\Models\Room;
 use Illuminate\Support\Facades\App;
+use App\Http\Controllers\BookingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,62 +23,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/booking', function () {
-    return view('booking')->with([
-        'beds'=>Bed::all(),
-        'procedures'=> Procedure::all()
-    ]);
-});
-
-Route::post('/booking', function () {
-    // validate
-    // save to table
-    // redirect
-    $data = request()->all();
-    //check booking date back
-    $currentdate = date('Y-m-d');
-
-    if ($data['datetime_start'] < $currentdate){
-        return back()->with('feedback', 'จองวันที่ย้อนหลังไม่ได้')->withInput();
-
-    }
-
-    //insert time to date
-    if ($data['time'] == 'morning'){
-        $datetime_start = $data['datetime_start'] . ' ' . '09:00:00';
-        $datetime_stop  = $data['datetime_start'] . ' ' . '12:00:00';
-     } else {
-         $datetime_start = $data['datetime_start'] . ' ' . '13:00:00';
-         $datetime_stop  = $data['datetime_start'] . ' ' . '16:00:00';
-     }
-     //check bed & datetime
-    $bookAlready = Booking::where('bed_id',$data['bed_id'])->where('datetime_start', $datetime_start)->count();
-
-
-    if ($bookAlready > 0){
-        return back()->with('feedback', 'เตียงนี้ถูกจองแล้ว')->withInput();
-
-    }
-
-    $booking = new Booking();
-    $booking->patient_id = 1;
-    $booking->bed_id = $data['bed_id'];
-    $bed = Bed::find($data['bed_id']);
-    $booking->room_id = $bed->room->id;
-    $booking->procedure_id = $data['procedure_id'];
-    $procedure = Procedure::find($data['procedure_id']);
-    $booking->clinic_id = $procedure->clinic->id;
-    // datetime_start
-    $booking->datetime_start = $datetime_start;
-    $booking->datetime_stop = $datetime_stop;
-
-    // week_day
-    $booking->week_day = now()->parse($data['datetime_start'])->weekDay();
-    $booking->user_id = 1;
-    $booking->save(); //insert data
-
-    return redirect()->route('home')->with('feedback', 'จองเตียงสำเร็จแล้ว');
-});
+Route::get('/booking', [BookingController::class, 'create']); // create-form
+Route::get('/booking/{id}', [BookingController::class, 'show']); // read
+Route::post('/booking', [BookingController::class, 'store']); // create-insert to table
 
 Route::get('/my-home', function () {
     return view('index');
@@ -167,8 +114,8 @@ Route::post('/room', function () {
 
 Route::get('/bed', function () {
     return view('bed')->with([
-        'beds'=> Bed::all(),
-        'rooms'=> Room::all()
+        //s'beds'=> Bed::all(),//ไม่ต้องส่งค่าเพราะไม่ได้ทำ drop list
+        'rooms'=> Room::all() // ส่งค่าเพื่อทำ drop list
     ]);
 
 });
@@ -176,24 +123,19 @@ Route::get('/bed', function () {
 Route::post('/bed', function () {
 
     $data = request()->all(); //รับมาจาก form
-    // return $data;
-    $nameAlready = Bed::where('no',$data['no'])->where('type', $data['type'])->count();
+    // $nameAlready = Bed::where('no',$data['no'])->where('type', $data['type'])->count();
+    $nameAlready = Bed::where('no',$data['no'])->where('room_id', $data['room_id'])->count();
 
 
     if ($nameAlready > 0){
-        return back()->with('feedback', 'หมายเลขเตียง/ชนิด/นี้มีแล้ว')->withInput();
+        return back()->with('feedback', 'หมายเลขเตียงนี้มีแล้ว')->withInput();
     }
 
     $bed = new Bed();
     $bed->no = $data['no'];
     $bed->type = $data['type'];
-    //$room = Room::find($data['room_id']);
-    // return $bed;
-    $bed->room;
     $bed->room_id = $data['room_id'];
     $bed->save();
-    // return $bed;
-    // return request()->all();
     return redirect()->route('home')->with('feedback', 'เพิ่มเตียงสำเร็จแล้ว');
 
 });
