1d593f71f9a2b5c8c863ee97d72a6023d364c685
saipin plumjit <nueng1157@gmail.com>
2022-08-05 09:28:13 -0700

    loginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\APIs\FakeAuthenticationAPI;
 use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class LoginController extends Controller
 {
@@ -21,7 +22,13 @@ class LoginController extends Controller
         $data = request()->all();
 
         $validated = request()->validate([
-            'username' => 'required',
+            // 'username' => 'required',
+            // 'password' => 'required',
+
+            'username' => [
+                'required',
+                Rule::unique('users')->where(fn($query) => $query->where('username',$data['username']))
+            ],
             'password' => 'required',
         ]);
 
@@ -29,18 +36,28 @@ class LoginController extends Controller
         $request->password = $validated['password'];
 
 
+
         $api = new FakeAuthenticationAPI;
-        $apiUser = $api->login($request->input('username'), $request->input('password'))->with('feedback', 'username & password เป็นค่าว่างไม่ได้ ');
+        // $apiUser = $api->login($request->input('username'), $request->input('password'))->with('feedback', 'username & password เป็นค่าว่างไม่ได้ ');
+        $apiUser = $api->login($request->input('username'), $request->input('password'));
+
+        // if ($apiUser > 0){
+        //     return back()->with('feedback', 'username & password เป็นค่าว่างไม่ได้ ')->withInput();
+        // }
+
 
         if (! $apiUser['found']) {
             // แจ้งให้ทราบว่าอะไรสักอย่างผิด
-            return 'user not found';
+            // return 'user not found';
+            return back()->with('feedback', 'ไม่มีข้อมูล user นี้ ')->withInput();
         }
 
         // check ตารางว่ามี user_name นี้หรือยัง ถ้ายังต้อง insert
         // SELECT * FROM users WHERE user_name = ?
         if ($user = User::where('username', $request->input('username'))->first()) {
-            return $user;
+            // return $user;
+            // return back()->with('feedback', 'xxx')->withInput();
+            return back()->with( $user);
         }
 
         return $apiUser;
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,7 +22,18 @@
                             <div class="alert alert-danger" role="alert">{{ session('feedback') }}</div>
                         @endif
 
-                       
+                         @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+
+
 
                     </div>
                     <div class="form-group">
