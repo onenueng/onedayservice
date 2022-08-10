bc059be5dfc94dec17815855000aa4f52feb13fb
onenueng <saipin.plu@mahidol.edu>
2022-08-10 13:17:16 +0700

    login controlloer

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\APIs\FakeAuthenticationAPI;
 use App\Models\User;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -19,50 +20,45 @@ class LoginController extends Controller
     public function login(Request $request)
     {
 
-        $data = request()->all();
-
-        $validated = request()->validate([
-            // 'username' => 'required',
-            // 'password' => 'required',
-
-            'username' => [
-                'required',
-                Rule::unique('users')->where(fn($query) => $query->where('username',$data['username']))
-            ],
-            'password' => 'required',
+        $validated = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
         ]);
 
-        $request->username = $validated['username'];
-        $request->password = $validated['password'];
-
-
-
         $api = new FakeAuthenticationAPI; //สร้าง API
-        // $apiUser = $api->login($request->input('username'), $request->input('password'))->with('feedback', 'username & password เป็นค่าว่างไม่ได้ ');
-        $apiUser = $api->login($request->input('username'), $request->input('password')); //รับ Input ที่ user ส่งมา
-
-        // if ($apiUser > 0){
-        //     return back()->with('feedback', 'username & password เป็นค่าว่างไม่ได้ ')->withInput();
-        // }
+        $apiUser = $api->login($validated['username'], $validated['password']);
 
 
-        if (! $apiUser['found']) {
-            // แจ้งให้ทราบว่าอะไรสักอย่างผิด
-            // return 'user not found';
-            // return back()->with('feedback', 'ไม่มีข้อมูล user นี้ ')->withInput();
-            return back()->with('feedback', 'ไม่พบข้อมูล user นี้ ');
+        if (! $apiUser['found']) { // AD บอกว่าไม่ผ่าน
+            return back()->with('feedback', 'ไม่พบข้อมูล user นี้ กรุณาลองใหม่');
         }
 
-        // check ตารางว่ามี user_name นี้หรือยัง ถ้ายังต้อง insert
-        // SELECT * FROM users WHERE user_name = ?
-        if ($user = User::where('username', $request->input('username'))->first()) {
-            // return back()->with('feedback','พบข้อมุล '.$user['username'] .' ในตาราง');
-            return back()->with('feedback',$user['username']);
-            // return back()->with( $user);
+        // AD ตอบว่าผ่านแล้ว
+        // หาว่ามี user ที่ใช้ username AD นี้ในตาราง users ของเราแล้วหรือยัง
+        // SELECT * FROM users WHERE username = ? LIMIT(1)
+        $user = User::where('username', $validated['username'])->first();
 
+        if ($user === null ) { // ยังไม่มี username นี้ในตาราง users ของเรา
+            // insert user ในตาราง users ของเรา
+            $user = new User;
+            $user->sap_id = $apiUser['sap_id'];
+            $user->username = $apiUser['username'];
+            $user->full_name = $apiUser['full_name'];
+            $user->password = \Hash::make(\Str::random(12));
+            $user->save();
+
+
+            // $user = User::create([
+            //     'sap_id' => $apiUser['sap_id'],
+            //     'username' => $apiUser['username'],
+            //     'full_name' => $apiUser['full_name'],
+            // ]);
+
+            // INSERT INTO users(...) VALUES(...)
         }
 
-        // return $apiUser;
-        return back()->with('feedback','ยังไม่พบข้อมุล '.$apiUser['username'] .' ในตาราง');
+        Auth::login($user); // laravel จะสร้าง session ให้
+
+        return redirect()->route('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,80 +29,62 @@ Route::get('/', function () {
     return redirect()->route('home');
 });
 
-Route::get('/booking', [BookingController::class,'index'])->name('booking');
-Route::get('/booking/create', [BookingController::class, 'create'])->name('booking.create'); // create-form
-Route::get('/booking/{booking}/edit', [BookingController::class,'edit'])->name('booking.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
-Route::post('/booking', [BookingController::class, 'store'])->name('booking.store'); // create-insert to table
-Route::delete('/booking/{booking}', [BookingController::class,'destroy'])->name('booking.destroy');
-Route::patch('/booking/{booking}', [BookingController::class,'update'])->name('booking.update');
-
-Route::get('/home', HomeController::class)->name('home');
-// Route::get('/home', [BookingController::class,'index'])->name('booking');
-
-Route::get('/user', [UserController::class,'index'])->name('user');
-Route::get('/user/{user}', [UserController::class,'show'])->name('user.show');
-Route::get('/user/{user}/edit', [UserController::class,'edit'])->name('user.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
-Route::patch('/user/{user}', [UserController::class,'update'])->name('user.update');
-Route::delete('/user/{user}', [UserController::class,'destroy'])->name('user.destroy');
-
-//Route::get('/login', UserController::class)->name('login');
-
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 
 
-Route::get('/patient', [PatientController::class,'index'])->name('patient');
-Route::get('/patient/{patient}', [PatientController::class,'show'])->name('patient.show');
-Route::get('/patient/{patient}/edit', [PatientController::class,'edit'])->name('patient.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
-Route::patch('/patient/{patient}', [PatientController::class,'update'])->name('patient.update');
-// Route::get('/patient', function()
-// {
-//     $patients = Patient::all();
+Route::middleware('auth')->group(function () {
+    Route::get('/home', HomeController::class)->name('home');
 
-//     return view('patient.index')->with([
-//         'patients'=> $patients
-//     ]);
+    Route::get('/booking', [BookingController::class,'index'])->name('booking');
+    Route::get('/booking/create', [BookingController::class, 'create'])->name('booking.create'); // create-form
+    Route::get('/booking/{booking}/edit', [BookingController::class,'edit'])->name('booking.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
+    Route::post('/booking', [BookingController::class, 'store'])->name('booking.store'); // create-insert to table
+    Route::delete('/booking/{booking}', [BookingController::class,'destroy'])->name('booking.destroy');
+    Route::patch('/booking/{booking}', [BookingController::class,'update'])->name('booking.update');
 
+    Route::get('/user', [UserController::class,'index'])->name('user');
+    Route::get('/user/{user}', [UserController::class,'show'])->name('user.show');
+    Route::get('/user/{user}/edit', [UserController::class,'edit'])->name('user.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
+    Route::patch('/user/{user}', [UserController::class,'update'])->name('user.update');
+    Route::delete('/user/{user}', [UserController::class,'destroy'])->name('user.destroy');
 
+    Route::get('/patient', [PatientController::class,'index'])->name('patient');
+    Route::get('/patient/{patient}', [PatientController::class,'show'])->name('patient.show');
+    Route::get('/patient/{patient}/edit', [PatientController::class,'edit'])->name('patient.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
+    Route::patch('/patient/{patient}', [PatientController::class,'update'])->name('patient.update');
 
-// });
+    Route::get('/clinic', [ClinicController::class,'index'])->name('clinic'); //เป็นหน้า index เพื่อแสดงข้อมูลทั้งหมด
+    Route::get('/clinic/create', [ClinicController::class,'create'])->name('clinic.create');
+    Route::get('/clinic/{clinic}/edit', [ClinicController::class,'edit'])->name('clinic.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
+    Route::get('/clinic/{clinic}', [ClinicController::class,'show'])->name('clinic.show');
+    Route::post('/clinic', [ClinicController::class,'store'])->name('clinic.store');
+    Route::delete('/clinic/{clinic}', [ClinicController::class,'destroy'])->name('clinic.destroy');
+    Route::patch('/clinic/{clinic}', [ClinicController::class,'update'])->name('clinic.update'); // ทำการ update ค่าจาก form ที่เรา  edit
 
+    Route::get('/procedure',[ProcedureController::class,'index'])->name('procedure');
+    Route::get('/procedure/create',[ProcedureController::class,'create'])->name('procedure.create');
+    Route::get('/procedure/{procedure}/edit', [ProcedureController::class,'edit'])->name('procedure.edit');
+    Route::get('/procedure/{procedure}', [ProcedureController::class,'show'])->name('procedure.show');
+    Route::post('/procedure',[ProcedureController::class,'store'])->name('procedure.store');
+    Route::delete('/procedure/{procedure}',[ProcedureController::class,'destroy'])->name('procedure.destroy');
+    Route::patch('/procedure/{procedure}',[ProcedureController::class,'update'])->name('procedure.update');
 
+    Route::get('/room',[RoomController::class,'index'])->name('room');
+    Route::get('/room/create',[RoomController::class,'create'])->name('room.create');
+    Route::get('/room/{room}/edit', [RoomController::class,'edit'])->name('room.edit');
+    Route::get('/room/{room}',[RoomController::class,'show'])->name('room.show');
+    Route::post('/room', [RoomController::class,'store'])->name('room.store');
+    Route::delete('/room/{room}', [RoomController::class,'destroy'])->name('room.destroy');
+    Route::patch('/room/{room}', [RoomController::class,'update'])->name('room.update');
 
-
-Route::get('/clinic', [ClinicController::class,'index'])->name('clinic'); //เป็นหน้า index เพื่อแสดงข้อมูลทั้งหมด
-Route::get('/clinic/create', [ClinicController::class,'create'])->name('clinic.create');
-Route::get('/clinic/{clinic}/edit', [ClinicController::class,'edit'])->name('clinic.edit'); //กดเข้าไปเพื่อเปิด form ที่มีข้อมูลของ record ที่เราต้องการ edit
-Route::get('/clinic/{clinic}', [ClinicController::class,'show'])->name('clinic.show');
-Route::post('/clinic', [ClinicController::class,'store'])->name('clinic.store');
-Route::delete('/clinic/{clinic}', [ClinicController::class,'destroy'])->name('clinic.destroy');
-Route::patch('/clinic/{clinic}', [ClinicController::class,'update'])->name('clinic.update'); // ทำการ update ค่าจาก form ที่เรา  edit
-
-Route::get('/procedure',[ProcedureController::class,'index'])->name('procedure');
-Route::get('/procedure/create',[ProcedureController::class,'create'])->name('procedure.create');
-Route::get('/procedure/{procedure}/edit', [ProcedureController::class,'edit'])->name('procedure.edit');
-Route::get('/procedure/{procedure}', [ProcedureController::class,'show'])->name('procedure.show');
-Route::post('/procedure',[ProcedureController::class,'store'])->name('procedure.store');
-Route::delete('/procedure/{procedure}',[ProcedureController::class,'destroy'])->name('procedure.destroy');
-Route::patch('/procedure/{procedure}',[ProcedureController::class,'update'])->name('procedure.update');
-
-Route::get('/room',[RoomController::class,'index'])->name('room');
-Route::get('/room/create',[RoomController::class,'create'])->name('room.create');
-Route::get('/room/{room}/edit', [RoomController::class,'edit'])->name('room.edit');
-Route::get('/room/{room}',[RoomController::class,'show'])->name('room.show');
-Route::post('/room', [RoomController::class,'store'])->name('room.store');
-Route::delete('/room/{room}', [RoomController::class,'destroy'])->name('room.destroy');
-Route::patch('/room/{room}', [RoomController::class,'update'])->name('room.update');
-
-Route::get('/bed', [BedController::class,'index'])->name('bed');
-Route::get('/bed/create', [BedController::class,'create'])->name('bed.create');
-Route::get('/bed/{bed}/edit', [BedController::class,'edit'])->name('bed.edit');
-Route::get('/bed/{bed}', [BedController::class,'show'])->name('bed.show');
-Route::post('/bed', [BedController::class,'store'])->name('bed.store');
-Route::delete('/bed/{bed}', [BedController::class,'destroy'])->name('bed.destroy');
-Route::patch('/bed/{bed}', [BedController::class,'update'])->name('bed.update');
-
-
-
+    Route::get('/bed', [BedController::class,'index'])->name('bed');
+    Route::get('/bed/create', [BedController::class,'create'])->name('bed.create');
+    Route::get('/bed/{bed}/edit', [BedController::class,'edit'])->name('bed.edit');
+    Route::get('/bed/{bed}', [BedController::class,'show'])->name('bed.show');
+    Route::post('/bed', [BedController::class,'store'])->name('bed.store');
+    Route::delete('/bed/{bed}', [BedController::class,'destroy'])->name('bed.destroy');
+    Route::patch('/bed/{bed}', [BedController::class,'update'])->name('bed.update');
+});
 
 
