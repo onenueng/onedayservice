fd0d98b2c11603a24ab36ae4fd921d380fbe15f7
onenueng <saipin.plu@mahidol.edu>
2023-02-17 15:22:40 +0700

    first commint on long time

--- resources/views/booking/information.blade.php
+++ resources/views/booking/information.blade.php
@@ -90,7 +90,7 @@
                     <td>1</td>
                     <td>0</td>
                     <td>1</td>
-                    <td>0</td>
+                    <td>1</td>
                 </tr>
             @endforeach
             </tbody>
